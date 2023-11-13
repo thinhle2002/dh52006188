97946265db712f963b9dd1bccb1a2b963cd784c7
thinhle2002 <dh52006188@student.stu.edu.vn>
2023-11-13 13:49:14 +0700

    kTraSoHoanThien

--- C++.cpp
+++ C++.cpp
@@ -1,6 +1,12 @@
 #include <iostream>;
 using namespace std;
-
+int kTSoHoanThien(int n){
+    int s = 0;
+    for(int i = 1; i<n;i++)
+        if(n%i == 0)
+	    s+ = i;
+    return s == n;
+}
 int main(){
     int a;
     cout<<"Nhap so nguyen";
